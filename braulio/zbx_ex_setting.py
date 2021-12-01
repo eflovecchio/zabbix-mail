93eaf23b12846ad678bab6d2bd78e020efad23a1
eflovecchio <95314924+eflovecchio@users.noreply.github.com>
2021-12-01 12:28:25 -0300

    Add files via upload

--- braulio/zbx_ex_setting.py
+++ braulio/zbx_ex_setting.py
@@ -3,21 +3,21 @@
 #Zabbix server settings
 #-----------------------------------------------
 zbx_server = 'http://xxxxxx'  # zabbix server full url
-zbx_api_user = 'xxxxxx'
-zbx_api_pass = 'xxxxxx'
+zbx_api_user = 'Admin'
+zbx_api_pass = 'admin'
 zbx_graph_width = '600'
 zbx_graph_height = '200'
 zbx_graph_period = '3600'
 
 #Email settings
-email_from = 'xxxx@xxxxxx'
-email_username = 'xxxxx@xxxxx'
-email_password = 'xxxxxxx'
+email_from = 'testzabbixosde@gmail.com'
+email_username = 'testzabbixosde@gmail.com'
+email_password = '79uwamaq'
 email_smtp_server = 'smtp.gmail.com'
 email_mail_port = '587'
 #------------------------------------------
 
-email_to = 'xxxxxxx@gmail.com'
+email_to = 'emanuelf.lovecchio@gmail.com'
 email_subject = 'graph'
 
 #Template settings
